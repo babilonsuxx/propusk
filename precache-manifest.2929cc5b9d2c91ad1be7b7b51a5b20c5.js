ebf482c0b6f5450007cc07025fab2bc27eacd541
babilonsuxx <babilonsuxx@gmail.com>
2020-04-29 00:42:44 +0300

    Updates

---
+++ precache-manifest.2929cc5b9d2c91ad1be7b7b51a5b20c5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86603b645c0171f72b53f3dc9105990a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad0632ace9d4ff90abb0",
+    "url": "/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "40ce8fbc3404479abb34",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "ad0632ace9d4ff90abb0",
+    "url": "/static/js/2.9dac35af.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/static/js/2.9dac35af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40ce8fbc3404479abb34",
+    "url": "/static/js/main.95746de9.chunk.js"
+  },
+  {
+    "revision": "1da1b54d0744929d3f45",
+    "url": "/static/js/runtime-main.c41c8d83.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
