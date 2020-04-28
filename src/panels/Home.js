adf22c27fe860f0288886fcdf8ea7fe2bb2f1417
babilonsuxx <babilonsuxx@gmail.com>
2020-04-29 02:29:26 +0300

    начал пилить форму

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -7,24 +7,46 @@ import Group from '@vkontakte/vkui/dist/components/Group/Group';
 import Cell from '@vkontakte/vkui/dist/components/Cell/Cell';
 import Div from '@vkontakte/vkui/dist/components/Div/Div';
 import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
+import { FormLayout, FormLayoutGroup, Input,Select } from '@vkontakte/vkui';
 
 const Home = ({ id, go, fetchedUser }) => (
 	<Panel id={id}>
-		<PanelHeader>Example</PanelHeader>
+		<PanelHeader>Введите данные11</PanelHeader>
 		{fetchedUser &&
-		<Group title="User Data Fetched with VK Bridge">
-			<Cell
-				before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null}
-				description={fetchedUser.city && fetchedUser.city.title ? fetchedUser.city.title : ''}
-			>
-				{`${fetchedUser.first_name} ${fetchedUser.last_name}`}
-			</Cell>
-		</Group>}
+			<Group title="User Data Fetched with VK Bridge">
+				<Cell
+					before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200} /> : null}
+					description={fetchedUser.city && fetchedUser.city.title ? fetchedUser.city.title : ''}
+				>
+					{`${fetchedUser.first_name} ${fetchedUser.last_name}`}
+				</Cell>
+			</Group>}
 
-		<Group title="Navigation Example">
+		<Group title="Введите данные">
+			<FormLayout>
+				<FormLayoutGroup top="Цель получения пропуска:">
+					<Select placeholder="работа/мед.организация/другое" id="targe1t">
+						<option value="1">Цифровой пропуск для работающих</option>
+						<option value="2">Разовая поездка для посещения медицинской организации</option>
+						<option value="3">Разовая поездка в иных целях</option>
+					</Select>
+				</FormLayoutGroup>
+			</FormLayout>
+
+			<FormLayout>
+				<FormLayoutGroup top="Тип документа, удостоверяющего личность:">
+				<Select placeholder="Паспорт/загран/другое">
+				<option value="1">Паспорт гражданина РФ</option>
+				<option value="2">Иностранный паспорт</option>
+				<option value="3">Иной документ</option>
+			</Select>
+				</FormLayoutGroup>
+			</FormLayout>
+
+			
 			<Div>
 				<Button size="xl" level="2" onClick={go} data-to="persik">
-					Show me the Persik, please
+					Скопировать
 				</Button>
 			</Div>
 		</Group>
